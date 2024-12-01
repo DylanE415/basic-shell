8be3a43a0cc61e77231750d28a6cc6be2c467e89
Dylan Eilber <82788786+DylanE415@users.noreply.github.com>
2024-11-30 20:47:41 -0800

    added a few commands

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pathlib
+import re
 
 
 def main():
@@ -8,29 +9,28 @@ def main():
 
     # Wait for user input
     while True:
-        sys.stdout.write("$ ")
+        sys.stdout.write("> ")
         sys.stdout.flush()
         full_command = input()
         command_args=full_command.split()
+        if len(command_args) == 0:
+            print("type help for all valid commands")
+            continue
         command=command_args[0]
+        arguments=separate_args_by_single_quote(command_args[1:])
+        print(arguments)
+        print(command_args[1:])
         if command in valid_commands:
             valid_commands[command](command_args[1:])
         elif find(command.split()[0]):
             os.system(full_command)
             continue
         else:
-            print(f"{command}: command not found")   
+            print(f"{command}: command not found, type help for all valid commands")   
 
 
 
-#helper function for the type command for finding a file
-def find(name):
-    path = os.environ['PATH']
-    for directory in path.split(':'):
-        full_path = os.path.join(directory, name)
-        if os.path.isfile(full_path):
-            return full_path
-    return None
+
         
 
 # command functions
@@ -70,6 +70,9 @@ def cd(*args):
     if args[0] ==[]:
         print("cd: missing argument")
         return None
+    if len(args[0]) > 1:
+        print("cd: too many arguments")
+        return None
     path = args[0][0]
     cwd = pathlib.Path(os.getcwd())
     if path == '~':
@@ -87,6 +90,31 @@ def cd(*args):
         except PermissionError:
             print(f"Permission denied to access {path}")
 
+def help(*args):
+    if args[0] ==[]:
+        print("all avaliable commands:")
+        for command in valid_commands:
+            print(command)
+        return None
+    else:
+        print("help: too many arguments")
+        return None
+    
+
+def cat(*args):
+    if args[0] ==[]:    
+        print("cat: missing argument")
+        return None
+    else:
+        for x in args[0]:
+            try:
+                with open(x) as f:
+                    print(f.read())
+            except FileNotFoundError:
+                print(f"cat: {x}: No such file or directory")
+   
+    
+
 #directory of valid commands to execute their respective functions
 valid_commands={
         "exit":exit,
@@ -94,7 +122,43 @@ valid_commands={
         "type":type_cmd,
         "pwd":pwd,
         "cd":cd,
+        "help":help,
+        "cat":cat
     }       
+
+
+#helper functions
+
+#to separate arguments by ''
+def separate_args_by_single_quote(args):
+    if len(args) == 0:
+        return []
+    args=args.join(" ")
+    single_quote_count = args.count("'")
+    
+    # Check for unmatched single quotes
+    if single_quote_count % 2 != 0:
+        print("Error: Unmatched single quotes in the input.")
+        return None
+    quoted_parts = re.findall(r"'(.*?)'", args)
+    
+    # Remove quoted parts from the string
+    stripped_string = re.sub(r"'(.*?)'", '', args).strip()
+    
+    # Split the remaining parts into arguments
+    remaining_args = stripped_string.split()
+
+    return quoted_parts, remaining_args
+
+
+#helper function for the type command for finding a file
+def find(name):
+    path = os.environ['PATH']
+    for directory in path.split(':'):
+        full_path = os.path.join(directory, name)
+        if os.path.isfile(full_path):
+            return full_path
+    return None
 
 if __name__ == "__main__":
     main()
